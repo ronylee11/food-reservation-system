17fec897635e1b6ce125e5e7e580e15bf65f5e4c
Rony Lee <rongyil33@gmail.com>
2024-02-07 18:38:02 +0800

    Add register post

---
+++ public/js/validateForms.js
@@ -0,0 +1,27 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+  const password = document.getElementById("password");
+  const password_confirmation = document.getElementById(
+    "password_confirmation"
+  );
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
