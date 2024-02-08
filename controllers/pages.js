10c98e6f92018bb4c8eb1de52fc1fb3ac9482c85
Rony Lee <rongyil33@gmail.com>
2024-02-08 13:11:27 +0800

    Add auto-route based on userType

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,4 +1,19 @@
 module.exports.index = (req, res) => {
+  // check userType and redirect to appropriate page
+  try {
+    if (req.user.userType && req.user.userType === "admin") {
+      return res.redirect("/admin");
+    } else if (req.user.userType === "canteenWorker") {
+      return res.redirect("/canteenWorker");
+    } else if (req.user.userType === "parent") {
+      return res.redirect("/parent");
+    } else {
+      res.render("index");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
   res.render("index");
 };
 
