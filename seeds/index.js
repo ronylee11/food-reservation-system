489c120ca8994053e7500a7a179a0e0a21110428
Rony Lee <rongyil33@gmail.com>
2024-02-08 12:34:15 +0800

    Add user seeds for admin and canteenWorker

---
+++ seeds/index.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const User = require("../models/user");
+
+mongoose.connect("mongodb://localhost:27017/food_reservation_system");
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected!");
+});
+
+const seedDB = async () => {
+  // clear all users
+  await User.deleteMany({});
+
+  // create admin user
+  const user = new User({
+    username: "admin",
+    userType: "admin",
+    phoneNumber: 1234567890,
+  });
+  await User.register(user, "admin");
+  console.log("Admin user created");
+
+  // create 3 canteen worker user
+  await User.register(
+    new User({
+      username: "canteenWorker1",
+      userType: "canteenWorker",
+      phoneNumber: 1234567890,
+    }),
+    "canteenWorker1"
+  );
+  console.log("Canteen worker 1 created");
+  await User.register(
+    new User({
+      username: "canteenWorker2",
+      userType: "canteenWorker",
+      phoneNumber: 1234567890,
+    }),
+    "canteenWorker2"
+  );
+  console.log("Canteen worker 2 created");
+  await User.register(
+    new User({
+      username: "canteenWorker3",
+      userType: "canteenWorker",
+      phoneNumber: 1234567890,
+    }),
+    "canteenWorker3"
+  );
+  console.log("Canteen worker 3 created");
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
