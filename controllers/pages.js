7276a3749f9c7191eca863749d310421ed2767a0
Rony Lee <rongyil33@gmail.com>
2024-02-08 14:39:14 +0800

    Suppress req.user undefined warning

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,14 +1,16 @@
 module.exports.index = (req, res) => {
   // check userType and redirect to appropriate page
   try {
-    if (req.user.userType && req.user.userType === "admin") {
-      res.render("admin/index");
-    } else if (req.user.userType === "canteenWorker") {
-      res.render("canteenWorker/index");
-    } else if (req.user.userType === "parent") {
-      res.render("parent/index");
-    } else {
-      res.render("index");
+    if (typeof req.user !== "undefined") {
+      if (req.user.userType && req.user.userType === "admin") {
+        res.render("admin/index");
+      } else if (req.user.userType === "canteenWorker") {
+        res.render("canteenWorker/index");
+      } else if (req.user.userType === "parent") {
+        res.render("parent/index");
+      } else {
+        res.render("index");
+      }
     }
   } catch (e) {
     console.log(e);
