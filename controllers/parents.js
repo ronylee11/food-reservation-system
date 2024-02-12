c7f612ee2a20f099243cbe14d36f52c2116cb40b
Rony Lee <rongyil33@gmail.com>
2024-02-12 22:23:21 +0800

    Add checking for account balance when withdraw

--- controllers/parents.js
+++ controllers/parents.js
@@ -19,14 +19,19 @@ module.exports.withdrawBalance = async (req, res) => {
   const userID = new ObjectId(req.user._id);
   const parent = await Parent.findOne({ user: userID });
   const { amount } = req.body;
-  parent.transactionHistory.push({
-    description: `Withdrawn ${amount}`,
-    balance: parent.accountBalance - parseInt(amount),
-    date: new Date(),
-  });
-  parent.accountBalance -= parseInt(amount);
-  parent.save();
-  res.render("parent/viewBalance", { accountBalance: parent.accountBalance });
+  if (parent.accountBalance < parseInt(amount)) {
+    req.flash("error", "Insufficient balance. Please top up your account.");
+    res.redirect("/");
+  } else {
+    parent.transactionHistory.push({
+      description: `Withdrawn ${amount}`,
+      balance: parent.accountBalance - parseInt(amount),
+      date: new Date(),
+    });
+    parent.accountBalance -= parseInt(amount);
+    parent.save();
+    res.render("parent/viewBalance", { accountBalance: parent.accountBalance });
+  }
 };
 
 module.exports.viewBalance = async (req, res) => {
