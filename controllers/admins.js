22b6b0981170444a776dcae72e2e881cff58e296
AUJIHIN <1211308797@student.mmu.edu.my>
2024-02-12 15:06:36 +0800

    Admin view top up history done

--- controllers/admins.js
+++ controllers/admins.js
@@ -1,3 +1,5 @@
+const Parent = require("../models/parent");
+
 module.exports.userData = (req, res) => {
     res.render("admin/userData");
   };
@@ -18,3 +20,12 @@ module.exports.userData = (req, res) => {
     res.render("admin/menuUpate");
   };
   
+  module.exports.viewTopUp = async (req, res) => {
+    try {
+      const topUpHistory = await Parent.find({}, { _id: 0, user: 1, transactionHistory: 1 }).populate('user', 'username');
+      res.render("admin/viewTopUp", { topUpHistory });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error" + error.message);
+    }
+  };
